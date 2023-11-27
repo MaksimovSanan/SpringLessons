5a279c7b4ee2fae0e284ce9b2544adb39c50cd0f
Song Rashida <songrash@at-o6.msk.21-school.ru>
2023-11-27 15:07:42 +0300

    batch-update

--- ex04/src/main/java/ru/maksimov/dao/PersonDAO.java
+++ ex04/src/main/java/ru/maksimov/dao/PersonDAO.java
@@ -1,12 +1,15 @@
 package ru.maksimov.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.maksimov.models.Person;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,6 +55,43 @@ public class PersonDAO {
         jdbcTemplate.update(
                 "DELETE FROM users WHERE id = ?",
                 id
+        );
+    }
+
+    public void batchUpdate(int quantity) {
+        List<Person> personList = new ArrayList<>();
+        for(int i = 0; i < quantity; ++i) {
+            personList.add(new Person(i, "name" + i, 30, "email" + i + "@mail.ru"));
+        }
+        jdbcTemplate.batchUpdate("INSERT INTO users(name, age, email) VALUES (?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setString(1, personList.get(i).getName());
+                        preparedStatement.setInt(2, personList.get(i).getAge());
+                        preparedStatement.setString(3, personList.get(i).getEmail());
+                    }
+                    @Override
+                    public int getBatchSize() {
+                        return personList.size();
+                    }
+                }
+        );
+    }
+
+    public void batchDelete(int begin, int end) {
+        jdbcTemplate.batchUpdate("DELETE FROM users WHERE id = ?",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, i + begin);
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return end - begin;
+                    }
+                }
         );
     }
 }

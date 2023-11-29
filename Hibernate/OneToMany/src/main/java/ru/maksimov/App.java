406e165c4fe822daa575a66cec710dd98e80a652
songrash <00-90->
2023-11-29 18:49:17 +0300

    one to many

--- Hibernate/OneToMany/src/main/java/ru/maksimov/App.java
+++ Hibernate/OneToMany/src/main/java/ru/maksimov/App.java
@@ -1,13 +1,47 @@
 package ru.maksimov;
 
+
+import net.bytebuddy.matcher.MethodOverrideMatcher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.maksimov.models.Movie;
+import ru.maksimov.models.Producer;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().
+                addAnnotatedClass(Producer.class).
+                addAnnotatedClass(Movie.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+
+//            Producer producer = session.get(Producer.class, 2);
+
+            Movie movie = session.get(Movie.class, 3);
+            System.out.println("Movie with id = 3:");
+            System.out.println(movie);
+
+            System.out.println("Producer of this movie:");
+            Producer producer = movie.getProducer();
+
+            System.out.println(producer);
+
+            System.out.println("All movies this producer:");
+            for(Movie anotherMovies : producer.getMovies()) {
+                System.out.println(anotherMovies);
+            }
+            session.getTransaction().commit();
+        }
     }
 }
